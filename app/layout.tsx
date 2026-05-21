9e05064de38a6d989b6739cf9ab6c2569c5c7ba0
D <dpoehlke@gmail.com>
2026-05-20 20:55:52 -0500

    feat: add Google Analytics (G-WX92WSVX1M) to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Bebas_Neue, IBM_Plex_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -93,6 +94,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WX92WSVX1M"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WX92WSVX1M');
+          `}
+        </Script>
+      </head>
       <body
         className={`${bebasNeue.variable} ${ibmPlexSans.variable} bg-marine-black text-silver antialiased`}
       >
